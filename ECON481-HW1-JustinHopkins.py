175525f4e8f4daa4161ab7e2a436007c8aac332c
jphopk <165586544+jphopk@users.noreply.github.com>
2024-03-31 13:06:50 -0700

    Update ECON481-HW1-JustinHopkins.py

---
+++ ECON481-HW1-JustinHopkins.py
@@ -0,0 +1,25 @@
+def github() -> str:
+    return "https://github.com/jphopk/ECON481-Homework/blob/main/ECON481-HW1-JustinHopkins.py"
+
+def evens_and_odds(n: int) -> dict:
+    evensSum = 0
+    oddsSum = 0
+    for i in range(0, n, 2):
+        evensSum += i
+    for i in range(1, n, 2):
+        oddsSum += i
+    return {'evens': evensSum, 'odds': oddsSum}
+
+
+from datetime import datetime, date, time, timedelta
+from typing import Union
+
+def time_diff(date_1: str, date_2: str, out: str) -> Union[str,float]:
+    dt1 = datetime.strptime(date_1, "%Y-%m-%d")
+    dt2 = datetime.strptime(date_2, "%Y-%m-%d")
+    delta = dt2-dt1
+    daysBtwn = abs(delta.days)
+    if (out == "string"):
+        return "There are " + str(daysBtwn) + " days between the two dates"
+    else:
+        return daysBtwn
